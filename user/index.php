2c29e291a4a98160a233b8fc6bd6246b52cf5002
Lê Quốc Thắng <thangle1121thanh@gmail.com>
2026-04-16 23:55:42 +0700

    update

--- user/index.php
+++ user/index.php
@@ -5,9 +5,9 @@
 <link rel="stylesheet" href="../assets/css/style_chung.css">
 
 <div style="min-height: 400px; padding: 50px; text-align: center; background-color: #f9f9f9;">
+    
     </div>
 
 <?php 
-    // Đường dẫn này trỏ đến file footer nằm trong thư mục includes
     include '../includes/footer.php'; 
 ?>
