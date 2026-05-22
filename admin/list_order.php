f442db4436f26e6a65653226ddce790bf0096988
thnh-dc <duoc4751050391@st.qnu.edu.vn>
2026-05-22 18:07:23 +0700

    update checkout
    Thêm xác nhận tự động cho đơn hàng theo hình thức chuyển khoản

--- admin/list_order.php
+++ admin/list_order.php
@@ -2,7 +2,9 @@
 session_start();
 require_once '../config/database.php';
 require_once __DIR__ . '/check_admin.php';
+require_once '../user/action_checkout/auto_cancel_unpaid_orders.php';
 
+autoCancelUnpaidBankOrders($pdo, 15);
 try {
     $user_filter = $_GET['user_id'] ?? '';
 
