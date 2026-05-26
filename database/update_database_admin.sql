baaf65878ead49a4b94929f17d5566af1f102253
thnh-dc <duoc4751050391@st.qnu.edu.vn>
2026-05-26 18:33:49 +0700

    update database

---
+++ database/update_database_admin.sql
@@ -0,0 +1,21 @@
+ALTER TABLE users ADD COLUMN admin_pin VARCHAR(255) DEFAULT NULL AFTER role;
+INSERT INTO users (
+    username, 
+    full_name, 
+    password, 
+    email, 
+    phone, 
+    role, 
+    admin_pin, 
+    status
+) 
+VALUES (
+    'admin', 
+    'Quản trị viên', 
+    '$2y$10$/OErnS/ifT0rIDHpxx581Ovbc76RBLiTQ0tiWhC8GgH5/9nY7P8VS', -- Hash của 'admin123'
+    'admin@gmail.com', 
+    '19001000', 
+    'admin', 
+    '$2y$10$evRk/NDcKBw/6BndbY0PJuLSGUtnuiIxtRXCFnNivMAol2aKnHfV6', -- Hash của '888888'
+    'active'
+);
