0f62c085f50c7c95bc86be82d50c363e94c21251
Lê Quốc Thắng <thangle1121thanh@gmail.com>
2026-05-30 16:29:54 +0700

    update

--- database/update_database_admin.sql
+++ database/update_database_admin.sql
@@ -12,10 +12,10 @@ INSERT INTO users (
 VALUES (
     'admin', 
     'Quản trị viên', 
-    '$2y$10$/OErnS/ifT0rIDHpxx581Ovbc76RBLiTQ0tiWhC8GgH5/9nY7P8VS', -- Hash của 'admin123'
+    '$2y$10$/OErnS/ifT0rIDHpxx581Ovbc76RBLiTQ0tiWhC8GgH5/9nY7P8VS',  -- Hash của 'admin123'
     'admin@gmail.com', 
     '19001000', 
     'admin', 
-    '$2y$10$evRk/NDcKBw/6BndbY0PJuLSGUtnuiIxtRXCFnNivMAol2aKnHfV6', -- Hash của '888888'
+    '$2y$10$evRk/NDcKBw/6BndbY0PJuLSGUtnuiIxtRXCFnNivMAol2aKnHfV6',  -- Hash của '888888'
     'active'
 );
